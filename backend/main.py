6a134906f61c43b700a69a5ce251802cf8d01d81
Aditya Patel <adityapatel@Adityas-MacBook.local>
2023-06-19 22:02:22 -0400

    loaddatabase() added

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle as pk
 import pandas as pd
+from datetime import date
 
 app = FastAPI()
 
@@ -12,13 +13,26 @@ class Metric(BaseModel):
     Low: float
     Vol: float
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class DateRange(BaseModel):
+    StartDate: str
+    EndDate: str
 
-@app.post('/heatingoil/')
+@app.post('/predict/')
 def heating_oil_prediction(metric: Metric):
     heating_oil_model = pk.load(open("heating_oil_model.pkl", "rb"))
     testing = pd.DataFrame({'Open':[metric.Open], 'High':[metric.High], 'Low':[metric.Low], 'Vol': [metric.Vol]})
     result = heating_oil_model.predict(testing)
     return ({"prediction:" : result[0]})
+
+@app.post('/getprices')
+def heating_oil_prices(daterange: DateRange):
+    return {"testing": "testing"}
+
+@app.post('/gethighlow')
+def heating_oil_highlow(daterange: DateRange):
+    return {"testing": "testing"}
+
+@app.get('/getscatterplot')
+def heating_oil_scatter():
+    return {"testing": "testing"}
+
